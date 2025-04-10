30cf7fe10671290fb614bd73d08cd883f73dfc6e
mikeleo03 <leonmichael463@gmail.com>
2025-04-10 22:08:20 +0700

    [Feat] Imbalanced dataset with 5 attributes over 3 parties

--- data/csv_gen.py
+++ data/csv_gen.py
@@ -15,5 +15,6 @@ def generate_csv(filename, n_samples=50, n_features=3, theta=None, noise_std=1.0
             writer.writerow(features + [target])
 
 # Generate CSVs
-generate_csv('data/party1.csv', n_samples=50, n_features=3, theta=[1.5, -2.0, 3.0])
-generate_csv('data/party2.csv', n_samples=60, n_features=3, theta=[1.5, -2.0, 3.0])
+generate_csv('data/party1_5.csv', n_samples=50, n_features=5, theta=[1.5, -2.0, -3.0, -6.7, 2.5])
+generate_csv('data/party2_5.csv', n_samples=60, n_features=5, theta=[-3.5, 3.0, 3.0, 16.7, 3.5])
+generate_csv('data/party3_5.csv', n_samples=200, n_features=5, theta=[4.5, -9.0, 0.0, 3.7, -6.0])
